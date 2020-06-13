ffd12543f83c438ec37b8848f521805ea60fb4fc
Micha Huhn <mihu111@gmx.de>
2020-06-13 03:51:38 +0200

    Class ToDoList, class Task and enum State (for Task) created.

---
+++ organizingTool-program/src/main/java/mainpackage/toDoList/Task.java
@@ -0,0 +1,55 @@
+package mainpackage.toDoList;
+
+/**
+ * The user can create as many tasks as he wants which will be displayed at the ToDoList.
+ */
+public class Task {
+
+    private String name;
+    private String content;
+    private int priority;
+    private String color;
+    private Date dueDate;
+    private Date reminder;
+    private State state = State.ACTIVE;
+
+    public Task(String name, String content, int priority, String color, Date dueDate, Date reminder) {
+        this.name = name;
+        this.content = content;
+        this.priority = priority;
+        this.color = color;
+        this.dueDate = dueDate;
+        this.reminder = reminder;
+    }
+
+    // Notiz: Überprüfen, ob Setter verwendet werden sollen oder ob in ToDoList ein neues Objekt erstellt werden soll, wenn es eine Änderung gibt.
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setReminder(Date reminder) {
+        this.reminder = reminder;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+}
